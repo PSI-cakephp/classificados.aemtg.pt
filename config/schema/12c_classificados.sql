770442063a645c36714a6acd706cef83ebdc9a0a
Gonçalo Correia <goncalo.correia@rupeal.com>
2017-02-22 21:56:35 +0000

    efetuadas alteracoes na bd nos valores default e o tamanho dos campos

--- config/schema/12c_classificados.sql
+++ config/schema/12c_classificados.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Máquina: localhost
--- Data de Criação: 21-Fev-2017 às 12:01
--- Versão do servidor: 5.5.52-MariaDB-1ubuntu0.14.04.1
--- versão do PHP: 5.5.9-1ubuntu4.20
+-- Host: 127.0.0.1
+-- Generation Time: 22-Fev-2017 às 21:35
+-- Versão do servidor: 5.7.14
+-- PHP Version: 7.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de Dados: `12c_classificados`
+-- Database: `12c_classificados`
 --
 CREATE DATABASE IF NOT EXISTS `12c_classificados` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `12c_classificados`;
@@ -28,8 +28,8 @@ USE `12c_classificados`;
 -- Estrutura da tabela `adds`
 --
 
-CREATE TABLE IF NOT EXISTS `adds` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `adds` (
+  `id` int(11) NOT NULL,
   `title` varchar(50) NOT NULL,
   `price` float NOT NULL,
   `description` varchar(255) NOT NULL,
@@ -38,9 +38,8 @@ CREATE TABLE IF NOT EXISTS `adds` (
   `modified` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
   `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
-  `type` enum('Venda','Compra','Troca') NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `type` enum('Venda','Compra','Troca') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -48,14 +47,13 @@ CREATE TABLE IF NOT EXISTS `adds` (
 -- Estrutura da tabela `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `icon` varchar(80) NOT NULL,
+  `icon` varchar(250) DEFAULT NULL,
   `parent` int(11) DEFAULT NULL,
-  `selectable` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `selectable` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -63,13 +61,12 @@ CREATE TABLE IF NOT EXISTS `categories` (
 -- Estrutura da tabela `images`
 --
 
-CREATE TABLE IF NOT EXISTS `images` (
+CREATE TABLE `images` (
   `id` int(11) NOT NULL,
   `link` varchar(255) NOT NULL,
   `adds_id` int(11) NOT NULL,
   `created` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
-  `modified` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
-  PRIMARY KEY (`id`)
+  `modified` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -78,14 +75,13 @@ CREATE TABLE IF NOT EXISTS `images` (
 -- Estrutura da tabela `rates`
 --
 
-CREATE TABLE IF NOT EXISTS `rates` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `rates` (
+  `id` int(11) NOT NULL,
   `rating` int(11) NOT NULL,
   `rated` int(11) NOT NULL,
   `rate` int(11) NOT NULL,
-  `created` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `created` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -93,23 +89,80 @@ CREATE TABLE IF NOT EXISTS `rates` (
 -- Estrutura da tabela `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(250) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
-  `profile_picture` varchar(80) NOT NULL,
+  `profile_picture` varchar(80) DEFAULT NULL,
   `mobile_number` varchar(50) NOT NULL,
-  `average_rating` varchar(50) NOT NULL,
-  `user_type` varchar(50) NOT NULL,
+  `average_rating` varchar(50) DEFAULT NULL,
+  `user_type` enum('user','admin') NOT NULL DEFAULT 'user',
   `status` enum('Ativado','Inativo','Bloqueado') NOT NULL DEFAULT 'Inativo',
   `hash` varchar(255) DEFAULT NULL,
-  `date_of_birth` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `date_of_birth` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adds`
+--
+ALTER TABLE `adds`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rates`
+--
+ALTER TABLE `rates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `adds`
+--
+ALTER TABLE `adds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT for table `rates`
+--
+ALTER TABLE `rates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
