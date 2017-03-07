c2eeda7523029b3aa5d3483b35fa21609f9d1205
Gonçalo Correia <goncalo.correia@rupeal.com>
2017-03-07 10:52:01 +0000

    adicionado o controlador de categorias com a vista das categorias

---
+++ src/Controller/Admin/CategoriesController.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Controller\AppController;
+use Cake\Utility\Text;
+use Cake\I18n\I18n;
+use Cake\ORM\TableRegistry;
+
+class CategoriesController extends AppController
+{
+
+    public function initialize()
+    {
+        // loads backend template to all methods
+		$this->viewBuilder()->layout('backend');
+    }
+
+    public function index(){
+        //die(debug($this->Categories->getSectionTree()));
+        $categories = $this->Categories->getSectionTree();
+        //$categories = TableRegistry::get('Categories');
+        //$categories->recover();
+        //$list = $categories->find('treeList',['spacer' => '<i>&nbsp;&nbsp;','valuePath' => 'name','keyPath'=>'icon']);
+        //die(debug($categories->find('treeList')));
+        $this->set('list',$categories);   //encontra e define as categorias
+    }
+
+    public function add(){
+        $this->set('categories',$this->Categories->find('list'));   //encontra e define as categorias na opçao parent
+
+        if($this->request->is('post')){   //espera por um pedido post
+
+            //--------------------------------------------------------------------------------------
+            //Handling do icon
+
+            $icon = $this->request->data('icon'); //recebe o valor do campo de upload
+            if($icon['name'] != ''){
+                $extension = pathinfo($icon['name'], PATHINFO_EXTENSION); //obtem a extensao do ficheiro
+                $icon['name'] = Text::uuid($icon['name']).'.'.$extension; //transforma o nome em uma string
+
+                move_uploaded_file($icon['tmp_name'], WWW_ROOT . 'img/categories_icon/' . $icon['name']);   //move o ficheiro para a pasta local
+            }
+
+
+            //---------------------------------------------------------------------------------------
+
+            $category = $this->Categories->newEntity($this->request->data());   //cria uma nova entidade
+            if($icon['name'] != ''){
+                $category->icon = 'categories_icon/' . $icon['name']; //guarda o nome e caminho do ficheiro na patch entity
+            }
+            else{
+                $category->icon = null;
+            }
+
+            if($this->Categories->save($category)){ //valida os dados
+                $this->Flash->success('Sucesso');
+                //faz qualquer coisa
+            }
+            else{
+                $this->Flash->error('Nao foi possivel guardar'); //envia uma mensagem de erro
+            }
+        }
+    }
+
+}
